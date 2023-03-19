f5f1c519a397240e390e5d39ce6335cb47dae12d
pnachasan <joanna.walowska@wp.pl>
2023-03-19 23:19:56 +0100

    publish message, but still not moving

---
+++ run_test/include/run_test/run_test_node.hpp
@@ -0,0 +1,51 @@
+// Copyright 2023 Joanna_Walowska
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RUN_TEST__RUN_TEST_NODE_HPP_
+#define RUN_TEST__RUN_TEST_NODE_HPP_
+
+// using std::placeholders::_1;
+#include <memory>
+#include <chrono>
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+
+#include "run_test/run_test.hpp"
+#include "autoware_auto_control_msgs/msg/ackermann_lateral_command.hpp"
+#include "autoware_auto_control_msgs/msg/longitudinal_command.hpp"
+#include "autoware_auto_control_msgs/msg/ackermann_control_command.hpp"
+#include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
+using namespace std::chrono_literals;
+
+namespace run_test
+{
+using RunTestPtr = std::unique_ptr<run_test::RunTest>;
+
+class RUN_TEST_PUBLIC RunTestNode : public rclcpp::Node
+{
+public:
+  explicit RunTestNode(const rclcpp::NodeOptions & options);
+
+private:
+  RunTestPtr run_test_{nullptr};
+  void foo();
+  void timer_callback();
+  rclcpp::Subscription<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr subscription_;
+  void get_topic(const geometry_msgs::msg::TwistWithCovarianceStamped::SharedPtr msg) const;
+  rclcpp::Publisher<autoware_auto_control_msgs::msg::AckermannControlCommand>::SharedPtr pub_ack;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+}  // namespace run_test
+
+#endif  // RUN_TEST__RUN_TEST_NODE_HPP_
---
+++ run_test/src/run_test_node.cpp
@@ -0,0 +1,68 @@
+// Copyright 2023 Joanna_Walowska
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "run_test/run_test_node.hpp"
+
+using namespace std::chrono_literals;
+namespace run_test
+{
+
+RunTestNode::RunTestNode(const rclcpp::NodeOptions & options)
+:  Node("run_test", options)
+{
+  run_test_ = std::make_unique<run_test::RunTest>();
+  const int64_t param_name = this->declare_parameter("param_name", 456);
+  run_test_->setParameters(param_name);
+
+  pub_ack = this->create_publisher<autoware_auto_control_msgs::msg::AckermannControlCommand>("/control/command/control_cmd", 10);
+  subscription_ = this->create_subscription<geometry_msgs::msg::TwistWithCovarianceStamped>("/sensing/vehicle_velocity_converter/twist_with_covariance", 10, std::bind(&RunTestNode::get_topic, this, std::placeholders::_1));
+  timer_ = this->create_wall_timer(500ms, std::bind(&RunTestNode::timer_callback, this));
+}
+
+void RunTestNode::timer_callback()
+{
+  std::cout << "hello" << std::endl;
+  auto message = autoware_auto_control_msgs::msg::AckermannControlCommand();
+  rclcpp::Time time_now = rclcpp::Clock().now();
+  message.stamp = time_now;
+  message.longitudinal.stamp = time_now;
+  message.lateral.stamp = time_now;
+  message.longitudinal.speed = 2.0;
+  message.longitudinal.acceleration = 2.0;
+  message.longitudinal.jerk = 0.5;
+  pub_ack->publish(message);
+}
+
+void RunTestNode::get_topic(const geometry_msgs::msg::TwistWithCovarianceStamped::SharedPtr msg) const
+{
+  std::cout << msg->twist.twist.linear.x << std::endl;
+}
+
+// void RunTestNode::get_topic(const geometry_msgs::msg::TwistWithCovarianceStamped::SharedPtr msg) const
+// {
+//   run_test_->printHello();
+// }
+
+void RunTestNode::foo()
+{
+  // run_test_->printHello();
+
+}
+
+}  // namespace run_test
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(run_test::RunTestNode)
