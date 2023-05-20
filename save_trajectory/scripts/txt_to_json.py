1c4b7252638c92345c0b14365e6a1ef8405d45a1
Cezary Wawrzyniak <cezary.waw99@gmail.com>
2023-05-20 23:15:15 +0200

    changing txt to json script to work as intended

--- save_trajectory/scripts/txt_to_json.py
+++ save_trajectory/scripts/txt_to_json.py
@@ -1,47 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[25]:
-
-
 import json
-
-
-# In[26]:
-
 
 with open('trajectory.txt') as f:
     lines = f.readlines()
-    
 
-
-# In[27]:
-
-
-X = []
-Y = []
+coordinates_list = []
 for i in lines:
-#     print(i.split())
-    X.append(float(i.split()[0]))
-    Y.append(float(i.split()[1]))
-
-
-# In[28]:
-
+    #     print(i.split())
+    point_dict = {"X": float(i.split()[0]), "Y": float(i.split()[1])}
+    coordinates_list.append(point_dict)
 
 dic = dict()
-dic = {"X": X, "Y": Y}
-
-
-# In[29]:
-
+dic = {"coords": coordinates_list}
 
 out_file = open("trajectory.json", "w")
-json.dump(dic, out_file, indent = 4, sort_keys = False)
+json.dump(dic, out_file, indent=4, sort_keys=False)
 out_file.close()
 
-
-# In[ ]:
 
 
 
